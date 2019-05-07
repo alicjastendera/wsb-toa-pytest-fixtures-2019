663bb8cb817afef41e4b01e983a150f9584aa24a
paweljedrzejczak <jedrzejczakpawel@gmail.com>
2019-05-08 01:28:08 +0200

    Fixture functions - wprowadzono poprawki dla mojego zakresu

--- function_example/conftest.py
+++ function_example/conftest.py
@@ -47,12 +47,12 @@ def create_board(credentials, logger):
 
 
 @pytest.fixture()
-def create_lists(credentials, logger, create_board, aviation_weather):
-    logger.info("Creating lists")
-    lists_url = URL + "lists"
+def create_list(credentials, logger, create_board, aviation_weather):
+    logger.info("Creating list")
+    list_url = URL + "lists"
     querystring = {"name": aviation_weather[1], "idBoard": create_board}
     querystring.update(credentials)
-    response = requests.post(lists_url, params=querystring)
+    response = requests.post(list_url, params=querystring)
 # Time sleep z uwagi na możliwość podglądu w UI TRELLO tworzenia listy z informacją pogodową tzw. "metar"
     time.sleep(5)
     return response
@@ -67,6 +67,7 @@ def aviation_weather(logger):
 # pilotów i uaktualniana co 30min. Na potrzeby tego testu "metar" pobierany jest z Lotniska Rębiechowo(skrót ICAO: EPGD)
     response = requests.get(metar_url, params=body)
     logger.info(response.text)
+    logger.info("metar for Gdańsk - Rębiechowo:")
     metar_item = response.json()["raw"]
     if response.status_code == HTTPStatus.OK:
         logger.info(metar_item)
--- function_example/test_create_lists_with_aero_weather_info.py
+++ function_example/test_create_lists_with_aero_weather_info.py
@@ -3,8 +3,8 @@ from http import HTTPStatus
 
 class Tests:
 
-    def test_create_lists(self, logger, create_lists):
-        assert create_lists.status_code == HTTPStatus.OK
+    def test_create_list(self, logger, create_list):
+        assert create_list.status_code == HTTPStatus.OK
 
     def test_aviation_weather(self, logger, aviation_weather):
         assert aviation_weather[0].status_code == HTTPStatus.OK
